965b1c267e5258e4a33ef07f491c4131d97ac122
J. Matt Peterson <jmatt@jmatt.org>
2014-04-24 16:28:46 -0700

    Up the version.
    	modified:   threepio/version.py

--- threepio/version.py
+++ threepio/version.py
@@ -4,7 +4,7 @@ Current logger version.
 from subprocess import Popen, PIPE
 from os.path import abspath, dirname
 
-VERSION = (0, 1, 2, 'dev', 0)
+VERSION = (0, 1, 3, 'dev', 0)
 
 def git_sha():
     loc = abspath(dirname(__file__))
